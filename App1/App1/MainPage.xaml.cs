0d158eef7c8b40d6386a24c58fe6d565ac21cad6
仁一 伊藤 <150215@jc-21.jp>
2017-11-16 14:31:32 +0900

    更新

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -13,5 +13,34 @@ namespace App1
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Double h = Double.Parse(Height.Text);
+            Double w = Double.Parse(Weight.Text);
+
+            //DisplayAlert("身長",h.ToString(),"OK");
+            //DisplayAlert("体重", w.ToString(), "OK");
+
+            if (h >= 3)
+            {
+                h /= 100;
+            }
+
+            double bmi = w / h / h;
+
+            if (bmi < 18.5)
+            {
+                DisplayAlert("BMI", "低体重", "OK");
+            }
+            else if (bmi < 25.0)
+            {
+                DisplayAlert("BMI", "普通体重", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", "肥満", "OK");
+            }
+        }
     }
 }
